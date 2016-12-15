dac5a403f4114984aee37c99e74988d1ed1cc082
kevloanda <kev5loanda@gmail.com>
2016-12-15 10:56:53 +0700

    Updated Mode

--- map.php
+++ map.php
@@ -37,7 +37,7 @@
                 font-family: Roboto;
                 font-size: 18px;
                 font-weight: 300;
-                margin-left: 12px;
+                margin-left: 30px;
                 padding: 0 11px 0 13px;
                 text-overflow: ellipsis;
                 width: 300px;
@@ -81,10 +81,11 @@
                         <div class="col-md-4">
                             <label style="font-size:30px"><a href="index.php"><img src="assets/medikuLogo.png" style="height: 40px"/>  Mediku<br></a> </label>                            
                         </div>
-                        <div class="col-md-3">                          
+                        <div class="col-md-4" style="text-align: center;">   
+                            <button class='btn-info' onclick='getData()'>Cari di Sekitar Saya</button>
                         </div>                       
-                        <div class="col-md-5" id="kanan">
-                            <label style="color:white;">Tugas Akhir Sistem Informasi Geografis People Power</label>                       
+                        <div class="col-md-4" id="kanan">
+                            <label style="color:white;">Tugas Akhir SIG People Power</label>                       
                         </div>
 
                     </div>
@@ -111,11 +112,13 @@
             </div>
         </div>
         <script>
+            var markers = [];
             var global;
             var startLat;
             var startLong;
             var map;
             var tmp;
+            var radius = 4;
             function initAutocomplete() {
                 map = new google.maps.Map(document.getElementById('map'), {
                     center: {lat: -6.4025, lng: 106.7942},
@@ -131,7 +134,7 @@
                             lat: position.coords.latitude,
                             lng: position.coords.longitude
                         };
-                        global = position.coords.latitude + "," + position.coords.longitude;
+                        global = position;
                         startLat = position.coords.latitude;
                         startLong = position.coords.longitude;
                         var icons = 'assets/loc64.png';
@@ -151,42 +154,69 @@
                     /*handleLocationError(false, infoWindow, map.getCenter());*/
                 }
 
-                $.getJSON("data.json", function (json) {
-                    var marker;
-                    var i = 0;
-                    for (var key in json) {
-                        if (json.hasOwnProperty(key)) {
-                            var icons = 'assets/hsp64.png';
-                            var marker = new google.maps.Marker({
-                                map: map,
-                                position: new google.maps.LatLng(json[key].geometry.location.lat, json[key].geometry.location.lng),
-                                icon: icons,
-                            });
-                            var geoLat = json[key].geometry.location.lat;
-                            var geoLng = json[key].geometry.location.lng;
-
-                            var content =
-                                    "<div style='width:180px;length:150px'><h4>" + json[key].name + "</h4>\n\
-                                <p>" + json[key].formatted_address + "</p><p style='text-align:center;color:grey'>" + geoLat + " , " + geoLng +
-                                    "<p style='text-align:center'><button class='btn btn-primary' onclick='directionFunction(" + geoLat + "," + geoLng + ")'>Direction</button></p>";
-
-                            var infowindow = new google.maps.InfoWindow()
-                            infowindow.open();
-
-                            google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
-                                return function () {
-                                    infowindow.setContent(content);
-                                    infowindow.open(map, marker);
-                                };
-                            })(marker, content, infowindow));
-                        }
-                    }
-                });
-
+//               
                 // Create the search box and link it to the UI element.
                 var input = document.getElementById('pac-input');
                 var searchBox = new google.maps.places.SearchBox(input);
                 map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+                // Bias the SearchBox results towards current map's viewport.
+                map.addListener('bounds_changed', function () {
+                    searchBox.setBounds(map.getBounds());
+                });
+
+                var markers = [];
+                // Listen for the event fired when the user selects a prediction and retrieve
+                // more details for that place.
+                searchBox.addListener('places_changed', function () {
+                    var places = searchBox.getPlaces();
+                    if (places.length == 0) {
+                        return;
+                    }
+
+                    // Clear out the old markers.
+                    markers.forEach(function (marker) {
+                        marker.setMap(null);
+                    });
+                    markers = [];
+
+                    // For each place, get the icon, name and location.
+                    var bounds = new google.maps.LatLngBounds();
+                    places.forEach(function (place) {
+                        if (!place.geometry) {
+                            console.log("Returned place contains no geometry");
+                            return;
+                        }
+                        startLat = place.geometry.location.lat();
+                        startLong = place.geometry.location.lng();
+                        var icon = {
+                            url: place.icon,
+                            size: new google.maps.Size(71, 71),
+                            origin: new google.maps.Point(0, 0),
+                            anchor: new google.maps.Point(17, 34),
+                            scaledSize: new google.maps.Size(25, 25)
+                        };
+
+                        // Create a marker for each place.
+                        markers.push(new google.maps.Marker({
+                            map: map,
+                            icon: icon,
+                            title: place.name,
+                            position: place.geometry.location
+                        }));
+                        if (place.geometry.viewport) {
+                            // Only geocodes have viewport.
+                            bounds.union(place.geometry.viewport);
+                        } else {
+                            bounds.extend(place.geometry.location);
+                        }
+                    });
+                    map.fitBounds(bounds);
+                });
+                // Create the search box and link it to the UI element.
+                var input = document.getElementById('pac-input');
+                var searchBox = new google.maps.places.SearchBox(input);
+                map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(input);
 
                 // Bias the SearchBox results towards current map's viewport.
                 map.addListener('bounds_changed', function () {
@@ -287,7 +317,7 @@
                 $.getJSON(string, function (data) {
                     tmp = data;
                 });
-                
+
                 // get route from A to B
                 calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB);
                 showDiv()
@@ -305,7 +335,7 @@
                         var distance = tmp.routes[0].legs[0].distance.text;
                         var duration = tmp.routes[0].legs[0].duration.text;
                         console.log(distance, duration)
-                        var dvDistance = document.getElementById("dvDistance");                         
+                        var dvDistance = document.getElementById("dvDistance");
                         dvDistance.innerHTML += "<div style='width:100%;background-color:white;font-size: 20px;padding:10px;text-align:center;'>Distance : " + distance + "<br>Duration : " + duration + "<br><br><a href='map.php' class='btn btn-primary'>Exit Direction Mode</a></div>";
                     } else {
                         window.alert('Directions request failed due to ' + status);
@@ -313,13 +343,79 @@
                 });
 
             }
-            
-            function showDiv(){
+
+            function showDiv() {
                 var x = document.getElementById("side");
                 var y = document.getElementById("dvDistance");
                 x.style.visibility = "visible";
                 y.style.visibility = "visible";
-        }
+            }
+
+            function getData() {
+                markers = [];
+                //clearMarkers();                
+                var count = 0;
+                $.getJSON("data.json", function (json) {
+                    var i = 0;
+                    for (var key in json) {
+                        if (json.hasOwnProperty(key)) {
+                            var icons = 'assets/hsp64.png';
+                            var geoLat = json[key].geometry.location.lat;
+                            var geoLng = json[key].geometry.location.lng;
+                            var distance = getDistance(geoLat, geoLng, startLat, startLong);
+                            var content =
+                                    "<div style='width:280px;'><h4>" + json[key].name + "</h4>\n\
+                                <p>" + json[key].formatted_address + "</p><p style='text-align:center;color:grey'>" + geoLat + " , " + geoLng +
+                                    "<p style='text-align:center'><button class='btn btn-primary' onclick='directionFunction(" + geoLat + "," + geoLng + ")'>Direction</button></p>";
+
+                            var infowindow = new google.maps.InfoWindow()
+                            infowindow.open();                            
+                            if (distance < radius) {
+                                var marker = new google.maps.Marker({
+                                    map: map,
+                                    position: new google.maps.LatLng(json[key].geometry.location.lat, json[key].geometry.location.lng),
+                                    icon: icons,
+                                });
+                                markers.push(marker);
+                                count += 1;
+                                google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+                                    return function () {
+                                        infowindow.setContent(content);
+                                        infowindow.open(map, marker);
+                                    };
+                                })(marker, content, infowindow));
+                            }
+                        }
+                    }
+                    if (count < 1) {
+                        alert("Tidak Ditemukan Rumah Sakit atau Puskesmas Terdekat");
+                    } else {
+                        alert("Ditemukan " + count + " Fasilitas Kesehatan di Sekitar Anda")
+                    }                    
+                });
+                deleteMarkers();
+            }
+            function deleteMarkers() {
+                clearMarkers();
+                markers = [];
+            }
+            function getDistance(p1, p2, p3, p4) {
+                var R = 6378137;
+                console.log(p1, p2, p3, p4);
+                var dLat = rad(p1 - p3);
+                var dLong = rad(p2 - p4);
+                var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                        Math.cos(rad(p1)) * Math.cos(rad(p3)) *
+                        Math.sin(dLong / 2) * Math.sin(dLong / 2);
+                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                var d = (R * c) / 1000;
+                //console.log(a, c, d);
+                return d;
+            }
+
+            var rad = function (x) {
+                return x * Math.PI / 180;
+            };
 
         </script>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDgDkjgoAZ7GbKexSSnDtIkBQUrLB6HHXw&libraries=places&callback=initAutocomplete"
